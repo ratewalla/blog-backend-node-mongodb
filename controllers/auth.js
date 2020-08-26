6b12eae6925578830d5dd0e14cdca6ffec2fd5d8
Rizwan Atewalla <rizwan_53@hotmail.com>
2020-08-26 11:26:06 +0100

    added login auth, tokens, signout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,22 @@
 const User = require('../models/user');
 const shortId = require('shortid');
+const expressJWT = require('express-jwt');
+const jwt = require('jsonwebtoken');
 
+
+
+
+// user sign up
 exports.signup = (req, res) => {
+
+    // check if user exists
    User.findOne({email: req.body.email}).exec((err, user)=>{
        if(user){
            return res.status(400).json({error: 'Email is taken.'})
        }
-   })
+   });
 
+    // create new user   
    const {name, email, password} = req.body;
    const username = shortId.generate();
    const profile = `${process.env.CLIENT_URL}/profile/${username}`;
@@ -20,9 +29,57 @@ exports.signup = (req, res) => {
        if(err){
            return res.status(400).json({error: err})
        }
-    //    res.json({message: 'Signup successfull. You can now sign in.'});
-       res.json({user: success});
+        res.json({message: 'Signup successfull. You can now sign in.'});
+        // res.json({user: success});
 
    })
 
 };
+
+// user sign in
+exports.signin = (req, res) => {
+
+    const {email, password} = req.body;
+
+    // check if user exists
+    User.findOne({email}).exec((err,user)=>{
+        if(err || !user){
+            return res.status(400).json({error: 'User with that email does not exist.'});
+        };
+    
+
+        // authenticate
+        if(!user.authenticate(password)){
+            return res.status(400).json({
+                error: 'Email and password dont match!'
+            });
+        }
+
+        // generate token and send to client
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {expiresIn: '1d'});
+
+        res.cookie('token', token, {expiresIn: '1d'});
+        const {_id, username, name, email, role} = user;
+        
+        return res.json({
+            token,
+            user: {_id, username, name, email, role}
+        });
+    });
+};
+
+
+// sign out
+exports.signout = (req, res) => {
+    res.clearCookie('token');
+    res.json({
+        message: 'Sign out Success'
+    });
+};
+
+
+// require login to view (need to set algorithms)
+exports.requireSignin = expressJWT({
+    secret: process.env.JWT_SECRET,
+    algorithms: ['HS256']
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const {signup} = require('../controllers/auth');
+const {signup,signin,signout, requireSignin} = require('../controllers/auth');
+
 
 // validators
 const {runValidation} = require('../validators')
 const {userSignupValidator} = require('../validators/auth')
+const {userSignInValidator} = require('../validators/auth')
 
 router.post('/signup', userSignupValidator, runValidation, signup);
+router.post('/signin', userSignInValidator, runValidation, signin);
+router.get('/signout', signout);
 
+// test authenticated page
+router.get('/secret', requireSignin,(req,res)=>{
+    res.json({
+        message: 'You have access to this secret page'
+    })
+})
 
 
 module.exports = router;
